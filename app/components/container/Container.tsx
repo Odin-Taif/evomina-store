6f20eba85380a89c1d3f3dcee3960bc64cfd56d5
Odin Thif <mjd.thif@icloud.com>
2023-12-15 19:25:46 +0100

    fixed the filter

--- app/components/container/Container.tsx
+++ app/components/container/Container.tsx
@@ -9,17 +9,17 @@ type Props = {};
 const Container = (props: Props) => {
   return (
     <div className="mb-[200px]">
-      {/* <div className="md:hidden">
+      <div className="md:hidden">
         <ResponsiveDrawer />
-      </div> */}
-      <div className="flex">
+      </div>
+      {/* <div className="flex">
         <div className="md:w-[250px] border-l-[0.5px] border-r-[0.5px] w-0 max-md:invisible">
           <Filter />
         </div>
         <div className="mx-auto px-auto md:px-4">
           <Item />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
