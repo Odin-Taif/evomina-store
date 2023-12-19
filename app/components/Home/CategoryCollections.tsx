5b92cef2567be9a503ffd163430f533e8cc1c286
Odin Thif <mjd.thif@icloud.com>
2023-12-19 02:25:55 +0100

    added new sections to the home page

--- app/components/Home/CategoryCollections.tsx
+++ app/components/Home/CategoryCollections.tsx
@@ -18,7 +18,7 @@ export default function CategoryCollections() {
                 alt="girl-image"
               />
               <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-                Women
+                Essential oil
               </button>
               <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
             </div>
@@ -30,7 +30,7 @@ export default function CategoryCollections() {
                   alt="shoe-image"
                 />
                 <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-                  Shoes
+                  Incense cones
                 </button>
                 <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
               </div>
@@ -41,7 +41,7 @@ export default function CategoryCollections() {
                   alt="watch-image"
                 />
                 <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-                  Watches
+                  Incense sticks
                 </button>
                 <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
               </div>
@@ -53,11 +53,13 @@ export default function CategoryCollections() {
                 alt="girl-image"
               />
               <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-                Women
+                Frankincense
               </button>
               <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
             </div>
-            <div className="relative group flex justify-center items-center h-full w-full mt-4 md:hidden md:mt-8 lg:hidden">
+
+            {/* small screen */}
+            <div className="relative group flex justify-center items-center h-60 w-full mt-4 md:hidden md:mt-8 lg:hidden">
               <img
                 className="object-center object-cover h-full w-full hidden md:block"
                 src="https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png"
@@ -69,12 +71,12 @@ export default function CategoryCollections() {
                 alt="olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2"
               />
               <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-                Women
+                Frankincense
               </button>
               <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
             </div>
           </div>
-          <div className="relative group hidden md:flex justify-center items-center h-full w-full mt-4 md:mt-8 lg:hidden">
+          <div className="relative group hidden md:flex justify-center items-center h-40 w-full mt-4 md:mt-8 lg:hidden">
             <img
               className="object-center object-cover h-full w-full hidden md:block"
               src="https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png"
@@ -86,7 +88,7 @@ export default function CategoryCollections() {
               alt="olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2"
             />
             <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-              Women
+              Frankincense
             </button>
             <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,19 @@ import Banner from "./components/Home/Banner";
 import CategoryCollections from "./components/Home/CategoryCollections";
 import Header from "./components/Home/Header";
 import NewsLetter from "./components/Home/NewLetter";
-
 import ContactSection from "./components/reusableComponents/ContactSection";
 import OurMission from "./components/reusableComponents/OurMission";
 
 export default async function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Banner />
       <div className="px-5 max-w-[1280px] mx-auto">
         <CategoryCollections />
         <hr className="my-4" />
-        <ContactSection />
         <OurMission />
+        <ContactSection />
         <NewsLetter />
       </div>
     </>
